761e679c628a7e081a28a4b97ca1ade3c3038ca0
Jeremy Martin <jeremymartin4794@gmail.com>
2023-02-24 14:23:18 -0500

    adding signup page

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 isLoggedIn = False
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html", isLoggedIn=isLoggedIn)
+    email = request.form.get("email")
+    return render_template("index.html", isLoggedIn=isLoggedIn, email=email)
 
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/signupResponse', methods=["POST"])
+def signupResponse():
+    firstName = request.form.get("firstName")
+    lastName = request.form.get("lastName")
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    return render_template("signupResponse.html", firstName=firstName, lastName=lastName, email=email, password=password), {"Refresh": "2; url=/login"}
 
 @app.route('/flashcard')
 def flashcard():
