904a3c6f2f77d7b87cd7ba5ed22c217ee2fe67cd
Jeremy Martin <jeremymartin4794@gmail.com>
2023-02-24 15:40:37 -0500

    adding error messages to signup form

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ isLoggedIn = False
 @app.route('/', methods=["GET", "POST"])
 def index():
     email = request.form.get("email")
+    password = request.form.get("password")
     return render_template("index.html", isLoggedIn=isLoggedIn, email=email)
 
 @app.route('/login')
@@ -23,8 +24,21 @@ def signupResponse():
     lastName = request.form.get("lastName")
     email = request.form.get("email")
     password = request.form.get("password")
+    passwordVerify = request.form.get("passwordVerify")
+    blankInputError = ""
+    passwordMismatchError = ""
 
-    return render_template("signupResponse.html", firstName=firstName, lastName=lastName, email=email, password=password), {"Refresh": "2; url=/login"}
+    if not firstName or not lastName or not email or not password or not passwordVerify:
+        blankInputError = "please fill in all fields"
+    
+    if password != passwordVerify:
+        passwordMismatchError = "passwords do not match"
+
+    if blankInputError or passwordMismatchError:
+        return render_template("signup.html", firstName=firstName, lastName=lastName, email=email, password=password, passwordVerify=passwordVerify,
+                               blankInputError=blankInputError, passwordMismatchError=passwordMismatchError)
+    else:
+        return render_template("signupResponse.html", firstName=firstName, lastName=lastName, email=email, password=password), {"Refresh": "2; url=/login"}
 
 @app.route('/flashcard')
 def flashcard():
